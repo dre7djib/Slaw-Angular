d15e49b6a0dda4e6f935cbd98cb7850fe6d40458
dre7djib <djibril.camara.pradier@gmail.com>
2024-05-15 12:15:44 +0200

    PDF Viewer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { LoginComponent } from './login/login.component';
+import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
 
 export const routes: Routes = [
     { path: 'users', component: UsersComponent },
+    { path: 'pdf', component: PdfViewerComponent},
     { path: 'auth/login', component: LoginComponent },
 ];
