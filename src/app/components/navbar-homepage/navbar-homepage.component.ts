dd7e14008af72340345ad718d6da75527914941e
kaan <kaantekten958@gmail.com>
2024-06-03 00:27:56 +0200

    avancement menu burger

--- src/app/components/navbar-homepage/navbar-homepage.component.ts
+++ src/app/components/navbar-homepage/navbar-homepage.component.ts
@@ -10,11 +10,22 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 export class NavbarHomepageComponent implements OnInit {
   public isLoggedIn: boolean;
+  public onClick: boolean;
 
   constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit() {
     const sessionStorage = this.document.defaultView?.sessionStorage;
     this.isLoggedIn = sessionStorage && sessionStorage.getItem('access_token') ? true : false;
+    this.onClick = false;
+  }
+
+  burgerClick() {
+    if (this.onClick) {
+      this.onClick = false;
+    }
+    else {
+      this.onClick = true
+    }
   }
 }
