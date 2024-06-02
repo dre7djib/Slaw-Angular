35fac5e62d5754a89f0b43c89a2a7c825cb5621c
kaan <kaantekten958@gmail.com>
2024-06-03 00:56:59 +0200

    avancement menu burger

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { OpenAiComponent } from './open-ai/open-ai.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 
 export const routes: Routes = [
@@ -16,5 +17,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'features', component: FeaturesComponent},
     { path: 'dashboard', component: DashboardComponent},
+    { path: 'profile', component: ProfileComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
