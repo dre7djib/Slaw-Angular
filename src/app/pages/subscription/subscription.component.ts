a1f1bd571218d9099217184cab31de3284e00141
dre7djib <djibril.camara.pradier@gmail.com>
2024-06-13 23:03:30 +0200

    Subscription Page

---
+++ src/app/pages/subscription/subscription.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarHomepageComponent } from '../../components/navbar-homepage/navbar-homepage.component';
+
+@Component({
+  selector: 'app-subscription',
+  standalone: true,
+  imports: [NavbarHomepageComponent],
+  templateUrl: './subscription.component.html',
+  styleUrl: './subscription.component.css'
+})
+export class SubscriptionComponent {
+
+}
